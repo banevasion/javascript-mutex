fe26f8094a158f0c6d10161799b003a93e28370c
banevasion <luka.debelic02@gmail.com>
2021-05-26 20:45:43 +0200

    Init

--- build/index.js
+++ build/index.js
@@ -8,43 +8,92 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-class Mutex {
-    constructor(content, maxAccesses = 0) {
+var Mutex = (function () {
+    function Mutex(content, maxAccesses) {
+        if (maxAccesses === void 0) { maxAccesses = 0; }
         this.content = content;
         this.maxAccesses = maxAccesses;
         this.locks = [];
         this.currentAccesses = 0;
     }
-    lock() {
-        return __awaiter(this, void 0, void 0, function* () {
-            var lock = () => { };
-            const lockPromise = new Promise((resolve) => (lock = resolve));
-            var unlock = () => { };
-            const unlockPromise = new Promise((resolve) => (unlock = resolve));
-            const lockObject = {
-                lock,
-                unlockPromise,
-            };
-            this.locks.push(lockObject);
-            this.processLock(lockObject);
-            yield lockPromise;
-            return { content: this.content, unlock };
-        });
-    }
-    processLock(lock) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (this.currentAccesses >= this.maxAccesses) {
-                while (this.locks[this.maxAccesses - 1] !== lock) {
-                    yield Promise.race(this.locks.map((lock) => lock.unlockPromise));
+    Mutex.prototype.lock = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var lock, lockPromise, unlock, unlockPromise, lockObject;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        lock = function () { };
+                        lockPromise = new Promise(function (resolve) { return (lock = resolve); });
+                        unlock = function () { };
+                        unlockPromise = new Promise(function (resolve) { return (unlock = resolve); });
+                        lockObject = {
+                            lock: lock,
+                            unlockPromise: unlockPromise,
+                        };
+                        this.locks.push(lockObject);
+                        this.processLock(lockObject);
+                        return [4, lockPromise];
+                    case 1:
+                        _a.sent();
+                        return [2, { content: this.content, unlock: unlock }];
                 }
-            }
-            this.currentAccesses++;
-            lock.lock();
-            yield lock.unlockPromise;
-            this.locks.splice(this.locks.indexOf(lock), 1);
-            this.currentAccesses--;
+            });
         });
-    }
-}
+    };
+    Mutex.prototype.processLock = function (lock) {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!(this.currentAccesses >= this.maxAccesses)) return [3, 3];
+                        _a.label = 1;
+                    case 1:
+                        if (!(this.locks[this.maxAccesses - 1] !== lock)) return [3, 3];
+                        return [4, Promise.race(this.locks.map(function (lock) { return lock.unlockPromise; }))];
+                    case 2:
+                        _a.sent();
+                        return [3, 1];
+                    case 3:
+                        this.currentAccesses++;
+                        lock.lock();
+                        return [4, lock.unlockPromise];
+                    case 4:
+                        _a.sent();
+                        this.locks.splice(this.locks.indexOf(lock), 1);
+                        this.currentAccesses--;
+                        return [2];
+                }
+            });
+        });
+    };
+    return Mutex;
+}());
 exports.default = Mutex;
