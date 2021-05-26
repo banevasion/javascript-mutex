d7edb0799935127ea679cdaa65146e405222b54e
banevasion <luka.debelic02@gmail.com>
2021-05-26 21:19:23 +0200

    Added generic

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,17 +1,17 @@
 declare type Lock = {
-    unlockPromise: Promise<void>;
-    lock: () => void;
+  unlockPromise: Promise<void>;
+  lock: () => void;
 };
-declare class Mutex {
-    maxAccesses: number;
-    private currentAccesses;
-    private locks;
-    private content;
-    constructor(content: any, maxAccesses?: number);
-    lock(): Promise<{
-        content: any;
-        unlock: () => void;
-    }>;
-    processLock(lock: Lock): Promise<void>;
+declare class Mutex<T> {
+  maxAccesses: number;
+  private currentAccesses;
+  private locks;
+  private content;
+  constructor(content: T, maxAccesses?: number);
+  lock(): Promise<{
+    content: T;
+    unlock: () => void;
+  }>;
+  processLock(lock: Lock): Promise<void>;
 }
 export default Mutex;
